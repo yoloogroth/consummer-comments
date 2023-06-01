b20369bd5a53a73c67d79a9e585f101b518e1a36
Yoloogroth <zS20020311@estudiantes.uv.mx>
2023-06-01 09:38:50 -0600

    update

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -8,8 +8,7 @@ from pymongo.server_api import ServerApi
 
 import json
 
-uri = "mongodb+srv://ygroth259:yolo123@cluster0.ah6hyg7.mongodb.net/?retryWrites=true&w=majority"
-
+uri = "mongodb+srv://ygroth259:yolo123@cluster0.ah6hyg7.mongodb.net/?retryWrites=true&w=majority";
 #Local = "mongodb://127.0.0.1:27017"
 #URL del profe = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
 
@@ -36,52 +35,48 @@ except:
     print("Could not connect to MongoDB")
 
 
-    consumer = KafkaConsumer('test',bootstrap_servers=[
-     ' my-kafka-0.my-kafka-headless.yoloogroth.svc.cluster.local:9092'
-    ])
-
-#'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'])
+consumer = KafkaConsumer('comments',bootstrap_servers=['my-kafka-0.my-kafka-headless.yoloogroth.svc.cluster.local:9092'])#'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'])
 # Parse received data from Kafka
+
 for msg in consumer:
     record = json.loads(msg.value)
     print(record)
     userId = record["userId"]
     objectId = record["objectId"]
-    reactionId = record["reactionId"]
+    comment = record["comment"]
 
     # Create dictionary and ingest data into MongoDB
     try:
-       reaction_rec = {
+        comment_rec = {
             'userId': userId,
             'objectId': objectId,
-            'reactionId': reactionId
+            'comment': comment
         }
-       print (reaction_rec)
-       reaction_id = db.memes_reactions.insert_one(reaction_rec)
-       print("Data inserted with record ids", reaction_id)
-    except:
-       print("Could not insert into MongoDB")
+        print(comment_rec)
+        comment_id = db.memes_comments.insert_one(comment_rec)
+        print("Comment inserted with record ids", comment_id)
+    except Exception as e:
+        print("Could not insert into MongoDB:")
 
-  # Create bdnosql_sumary and insert groups into mongodb
+    # Create bdnosql_sumary and insert groups into mongodb
     try:
-        agg_result = db.memes_reactions.aggregate([
-        {
-            "$group": {
+        agg_result = db.memes_comments.aggregate([
+              {
+         "$group": {
                 "_id": {
                     "objectId": "$objectId",
-                    "reactionId": "$reactionId"
+                    "comment": "$comment"
                 },
                 "n": {"$sum": 1}
             }
         }
     ])
-        db.memes_sumaryReactions.delete_many({})
+
+        db.memes_sumaryComments.delete_many({})
         for i in agg_result:
             print(i)
-            sumary_id = db.memes_sumaryReactions.insert_one(i)
-            print("Sumary Reactions inserted with record ids: ", sumary_id)
-            
+            sumaryComments_id = db.memes_sumaryComments.insert_one(i)
+            print("Sumary Comments inserted with record ids: ", sumaryComments_id)
     except Exception as e:
         print(f'group vy cought {type(e)}: ')
         print(e)
-
